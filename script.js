e74a1d3d68d1448d04175f90903f7c28fbc398cf
Ke1ee <Kelee1590@gmail.com>
2023-07-17 00:00:28 -0400

    Added JS functions to fetch weather using inputted parameters.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Create object to store functions for API
  let weather = {
-    "apiKey": "5c3194248aa07cc1818a2aee947f3e0d"
-    
+    "apiKey": "5c3194248aa07cc1818a2aee947f3e0d",
+    fetchWeather: function (city) {
+      fetch(
+         "https://api.openweathermap.org/data/2.5/weather?q=" 
+      + city 
+      + "&units=imperial&appid=" 
+      + this.apiKey
+   )
+      .then((response) => response.json())
+      .then((data) => console.log(data)); 
+    },
+    displayWeather: function(data) {
+
+    }
+
  }
